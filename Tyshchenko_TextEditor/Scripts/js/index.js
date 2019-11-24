8d8017a53142f78a61fd8db9443eef98e3d39af7
dashatyshch <dasha.tyshenko98@gmail.com>
2019-11-24 16:41:51 +0200

    Added file saving (front)

--- Tyshchenko_TextEditor/Scripts/js/index.js
+++ Tyshchenko_TextEditor/Scripts/js/index.js
@@ -10,6 +10,7 @@
         function (e) {
             e.preventDefault();
             var file = $(this)[0].files[0];
+
             //TODO: check file extension
 
             //var formData = new FormData();
@@ -21,14 +22,36 @@
                 processData: false,
                 contentType: false,
                 success: function (data) {
-                    var fr = new FileReader();
-                    fr.onload = function () {
-                        $("#document-area").val(this.result);
-                    };
+                    var reader = new FileReader();
+                    reader.onload = function (event) {
+                        $("#document-area").val(event.target.result);
+                    }
+                    name = e.target.files[0].name;
+                    reader.readAsText(new Blob([e.target.files[0]], {
+                        "type": "application/json"
+                    }));
 
-                    fr.readAsText(file);
+                    //fr.readAsText(file);
                 }
             });
         });
+
+    $("#save_file").on("click",
+        function (e) {
+            e.preventDefault();
+            var blob = new Blob([$("#document-area").val()], {
+                "type": "application/json"
+            });
+
+            var a = document.createElement("a");
+            a.download = name;
+            a.href = URL.createObjectURL(blob);
+            document.body.appendChild(a);
+            a.click();
+            $("#document-area").val("");
+            $("#upload-input").value = "";
+            document.body.removeChild(a);
+        }
+    );
 
 })
